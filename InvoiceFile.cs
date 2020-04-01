28974948af0ce5b0394bc1bfd4e0681da8cf55e7
Matt Obert on Windows <matt@hotwebmatter.com>
2020-03-31 23:12:57 -0400

    Add class to handle file IO

---
+++ InvoiceFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RegalCollections
+{
+    class InvoiceFile
+    {
+        public void WriteFile(InvoiceCollection invoices)
+        {
+            StreamWriter outfile;
+            outfile = File.CreateText("invoices.txt");
+            foreach (Invoice anInvoice in invoices.AllInvoices)
+            {
+                outfile.Write(anInvoice.number);
+                outfile.Write(",");
+                outfile.Write(anInvoice.custID);
+                outfile.Write(",");
+                outfile.Write(anInvoice.date.ToShortDateString());
+                outfile.Write(",");
+                outfile.Write(anInvoice.salesAmount);
+            }
+        }
+        public void ReadFile(InvoiceCollection invoices)
+        {
+            StreamReader infile;
+            char delimiter = ',';
+            string line;
+            string[] fields = new string[4];
+            if (File.Exists("invoices.txt"))
+            {
+                infile = File.OpenText("invoices.txt");
+                while (!infile.EndOfStream)
+                {
+                    line = infile.ReadLine();
+                    fields = line.Split(delimiter);
+                    Invoice anInvoice = new Invoice;
+                    anInvoice.number = int.Parse(fields[0]);
+                    anInvoice.custID = int.Parse(fields[1]);
+                    anInvoice.date = DateTime.Parse(fields[2]);
+                    anInvoice.salesAmount = double.Parse(fields[3]);
+                    invoices.AddInvoice(anInvoice);
+                }
+                infile.Close();
+            }
+        }
+    }
+}
