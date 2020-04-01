ee316b0de54883b6d1406902c1f797f4a8c0ac92
Matt Obert on Windows <matt@hotwebmatter.com>
2020-03-31 21:40:32 -0400

    Create Invoice class

---
+++ Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegalCollections
+{
+    public class Invoice
+    {
+        public int number;
+        public int custID;
+        public DateTime date;
+        public double salesAmount;
+    }
+}
