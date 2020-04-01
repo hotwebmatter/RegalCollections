6a7d3a834d888e21bde73a51609815f3d9da0eff
Matt Obert on Windows <matt@hotwebmatter.com>
2020-03-31 21:52:00 -0400

    Stub out InvoiceCollection class

---
+++ InvoiceCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegalCollections
+{
+    public class InvoiceCollection
+    {
+        // dictionary
+        Dictionary<int, Invoice> invoiceList = new Dictionary<int, Invoice>();
+
+        // value collection
+        public Dictionary<int, Invoice>.ValueCollection AllInvoices
+        {
+            get { return invoiceList.Values; }
+        }
+    }
+}
