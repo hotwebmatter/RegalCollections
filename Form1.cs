03ffde8c00acd3ea64e44888cd789637c04793db
Matt Obert on Windows <matt@hotwebmatter.com>
2020-04-01 10:28:44 -0400

    Wire up exit click handler in menu

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace RegalCollections
         {
             InitializeComponent();
         }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close(); // TODO: Prompt user to confirm exit
+        }
     }
 }
