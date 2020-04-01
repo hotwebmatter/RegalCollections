2e1cc0bdd76b7ad445e4977ff5f07ffc3dcbdc62
Matt Obert on Windows <matt@hotwebmatter.com>
2020-04-01 10:29:47 -0400

    Set public readonly modifiers on dictionary

--- InvoiceCollection.cs
+++ InvoiceCollection.cs
@@ -9,7 +9,7 @@ namespace RegalCollections
     public class InvoiceCollection
     {
         // dictionary property
-        Dictionary<int, Invoice> invoiceList = new Dictionary<int, Invoice>();
+        public readonly Dictionary<int, Invoice> invoiceList = new Dictionary<int, Invoice>();
 
         // value collection method (accessor)
         public Dictionary<int, Invoice>.ValueCollection AllInvoices
